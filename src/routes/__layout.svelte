9d68f6f7e70988f9bb43c0508b31b24cd4b15b73
leovoon <voonlihhaur@gmail.com>
2022-06-25 19:16:39 +0800

    initial skeleton setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="w-full h-screen grid place-content-center ">
+	<slot />
+</main>
+
+<footer>made with ❤️ by leovoon for her</footer>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1 class="text-center w-full mb-4 ">Release <sup>beta 0.0.1</sup></h1>
+
+<div class="flex flex-col gap-4">
+	<a href="/hate" class="btn-blue"> I hate it when</a>
+	<a href="/happy" class="btn-red">I am happy that</a>
+</div>
+
+<button class="btn-light">view history</button>
