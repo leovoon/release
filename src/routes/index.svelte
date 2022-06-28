44793d29ab5244098b4570e17e9404828eb76672
leovoon <voonlihhaur@gmail.com>
2022-06-28 13:03:41 +0800

    fix swapped query

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,8 @@
 <h1 class="text-center w-full mb-4 ">Release <sup>beta 0.0.1</sup></h1>
 
 <div class="flex flex-col gap-4">
-	<a href="/compose?m=happy" class="btn-blue"> I hate it when</a>
-	<a href="/compose?m=hate" class="btn-red">I am happy that</a>
+	<a href="/compose?m=hate" class="btn-blue"> I hate it when</a>
+	<a href="/compose?m=happy" class="btn-red">I am happy that</a>
 </div>
 
 <a href="/history" class="btn-light">view history</a>
