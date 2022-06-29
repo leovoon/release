05fc504914570314741dc27cdc6848c32b5385c6
leovoon <voonlihhaur@gmail.com>
2022-06-29 21:26:20 +0800

    test google login

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.css';
+	import { session, page } from '$app/stores';
+	import { signOut as authSignOut } from 'sk-auth/client';
+
+	let notice = '';
+
+	function signOut() {
+		authSignOut()
+			.then(session.set)
+			.then(() => handleIsLoggedOut());
+	}
+
+	function handleIsLoggedOut() {
+		notice = 'You logged out.';
+		setTimeout(() => (notice = ''), 1000);
+	}
 </script>
 
 <svelte:head>
@@ -7,6 +22,13 @@
 </svelte:head>
 
 <main>
+	{notice}
+	{#if $session?.user}
+		<a href="/">You logged in. </a>
+		<button on:click={signOut} class="btn-login mb-12">Sign out</button>
+	{:else if $page.url.pathname !== '/login'}
+		<a href="/login" class="btn-login mb-14">Login</a>
+	{/if}
 	<slot />
 </main>
 
---
+++ src/routes/privacy/index.svelte
@@ -0,0 +1,5 @@
+<h1>Privacy Policy</h1>
+<p>
+	Here is your consent that Release does not collect your data when you view
+	this page. This includes when you log in with Google.
+</p>
