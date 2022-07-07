4365b1c703b99aab9fe348656160bd513d18358d
leovoon <voonlihhaur@gmail.com>
2022-07-07 11:10:41 +0800

    fix netlify build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify@1.0.0-next.65';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
